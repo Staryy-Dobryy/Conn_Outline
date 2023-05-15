86b02c42c237c83c3aed138159f670c667e035ca
Staryy-Dobryy <lfcega1@gmail.com>
2023-05-15 22:39:40 +0300

    Added BL layer

---
+++ ConnOutlineMessenger.BuisnessLogic/Services/Injecting.cs
@@ -0,0 +1,34 @@
+﻿using ConnOutlineMessenger.DBstur;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ConnOutlineMessenger.DataBaseStartup
+{
+    public static class Injecting
+    {
+        public static void AddDataBase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(configuration["ConnectionString"]));
+            services.AddScoped<DataBaseContext>();
+        }
+        public static void UseDataBase(this IServiceProvider services)
+        {
+            using (var scope = services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<DataBaseContext>();
+                    context.Database.EnsureDeleted();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception exception)
+                {
+                    throw exception;
+                }
+            }
+        }
+    }
+}
