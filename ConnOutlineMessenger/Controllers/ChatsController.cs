ce3512848fe1113c019aad85bed8585eebd562c1
Staryy-Dobryy <lfcega1@gmail.com>
2023-05-15 22:41:50 +0300

    Few frontend changes

---
+++ ConnOutlineMessenger/Controllers/ChatsController.cs
@@ -0,0 +1,28 @@
+﻿using ConnOutlineMessenger.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ConnOutlineMessenger.Controllers
+{
+    public class ChatsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ChatsController (ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
