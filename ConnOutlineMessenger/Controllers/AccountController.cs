065aaed88fcf7a619a6434d8e575fe94564b99e9
Staryy-Dobryy <lfcega1@gmail.com>
2023-05-19 19:53:48 +0300

    PIL update

---
+++ ConnOutlineMessenger/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using ConnOutlineMessenger.BuisnessLogic.Interfaces;
+using ConnOutlineMessenger.BuisnessLogic.Services;
+using ConnOutlineMessenger.DTO;
+using ConnOutlineMessenger.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace ConnOutlineMessenger.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly IAccountService _accountService;
+
+        public AccountController(ILogger<AccountController> logger, IAccountService accountService)
+        {
+            _accountService = accountService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpGet]
+        public IActionResult Registration() => View();
+        [HttpPost]
+        public async Task<IActionResult> Registration(RegistrationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _accountService.Register(model);
+                return View(model);
+            }
+
+            model.Modal = true;
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult FastRegistration() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- ConnOutlineMessenger/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ConnOutlineMessenger.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
