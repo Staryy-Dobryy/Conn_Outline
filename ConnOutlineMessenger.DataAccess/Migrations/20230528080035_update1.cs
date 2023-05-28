1b4bd1009965f0293193b11fc772792cbae44d14
Staryy-Dobryy <lfcega1@gmail.com>
2023-05-28 22:39:42 +0300

    Added migrations

---
+++ ConnOutlineMessenger.DataAccess/Migrations/20230528080035_update1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConnOutlineMessenger.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ChatName",
+                table: "Chats",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChatName",
+                table: "Chats");
+        }
+    }
+}
